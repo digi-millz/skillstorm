14496b469fac2e4ca4f39d02e536b12ec6b268f0
Millz <myer.millien@gmail.com>
2025-02-11 05:54:31 -0500

    dataStructure

---
+++ java/myapp/src/ArrayLists.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class ArrayLists {
+    public static void main(String[] args) {
+               ArrayList<String> names = new ArrayList<String>();
+        // print how many Strings are in the list
+        System.out.println(names.size());
+
+
+
+    }
+}
---
+++ java/myapp/src/Declaring.java
@@ -0,0 +1,12 @@
+// import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Declaring {
+    public static void main(String[] args) {
+        // read(); // throws FileNotFoundException but must be handled elsewhere
+    }
+
+    public static void read() throws FileNotFoundException {
+        // FileInputStream file = new FileInputStream("file.txt");
+    }
+}
